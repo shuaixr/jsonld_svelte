79763e143d3e3cb308780db46fdc920d5dd47ec0
shuaixr <1025sxr@gmail.com>
2023-01-31 11:17:49 +0800

    init

---
+++ src/lib/JSONLD.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let json_ld: unknown;
+</script>
+
+<svelte:head
+	>{@html `
+    <script type="application/ld+json">
+            ${JSON.stringify(json_ld)}
+    </script>`}
+</svelte:head>
